803dcf11690e9d7c9ab0d56c7a11e7432833ea02
Darren <darrens@hotmail.fr>
2018-11-06 16:13:59 +0100

    Connecting to MongoDB

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
 console.log('Server-side code running');
 
 const express = require('express');
-
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 // serve files from the public directory
 app.use(express.static('public'));
 
-// start the express web server listening on 8080
-app.listen(8080, () => {
-    console.log('listening on 8080');
+// connect to the db and start the express server
+let db;
+
+// ***Replace the URL below with the URL for your database***
+const url = 'mongodb://Mongodab:azerty69@ds253203.mlab.com:53203/mongodab_test';
+// E.g. for option 2) above this will be:
+// const url =  'mongodb://localhost:21017/databaseName';
+
+MongoClient.connect(url, (err, database) => {
+    if (err) {
+        return console.log(err);
+    }
+    db = database;
+    // start the express web server listening on 8080
+    app.listen(8080, () => {
+        console.log('listening on 8080');
+    });
 });
 
 // serve the homepage
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+// add a document to the DB collection recording the click event
+app.post('/clicked', (req, res) => {
+    const click = {
+        clickTime: new Date()
+    };
+    console.log(click);
+    console.log(db);
+
+    db.collection('clicks').save(click, (err, result) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('click added to db');
+        res.sendStatus(201);
+    });
 });
