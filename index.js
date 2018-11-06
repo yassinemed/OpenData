6e3affd4ef399195585fb1da0f3e6728c09be162
MarcWichegrod <ms.wichegrod@yahoo.fr>
2018-11-06 11:59:30 +0100

    premier test cards

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ const request = require('request')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+	reqURL = "https://raw.githubusercontent.com/nodanaonlyzuul/against-humanity/master/source/cards.json"
+	request(reqURL,
+		function(error, response, body) {
+			
+			res.send(body)
+		})
 })
 
 app.get('/toto', function (req, res) {
-  res.send('Hello Toto!')
-} )
+  	res.send('Hello Toto!')
+})
 
 port = process.env.PORT || 8080
 
 app.listen(port, function () {
   console.log('Example app listening on port '+port)
 })
-
-
-
