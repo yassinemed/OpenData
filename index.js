a4d42bb01def47423c3c230da7000d22265b6032
Sylvain Maillot <sylvain.maillot1@gmail.com>
2018-11-06 09:12:21 +0100

    Node init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/toto', function (req, res) {
+  res.send('Hello Toto!')
+} )
+
+port = process.env.PORT || 8080
+
+app.listen(port, function () {
+  console.log('Example app listening on port '+port)
+})
+
+
+
