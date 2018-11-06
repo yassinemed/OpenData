45c3b4d1b2008dbd079280c2d4b9345a3df597cf
Darren <darrens@hotmail.fr>
2018-11-06 13:09:03 +0100

    getData.html in server.js

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ const request = require('request')
 //test
 const app = express()
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/getData.html');
+});
+
+
+app.get('/getcards', function (req, res) {
 	reqURL = "https://raw.githubusercontent.com/nodanaonlyzuul/against-humanity/master/source/cards.json"
 	request(reqURL,
-		function(error, response, body) {
-			
+		function (error, response, body) {
+
 			res.send(body)
 		})
 })
 
 app.get('/toto', function (req, res) {
-  	res.send('Hello Toto!')
+	res.send('Hello Toto!')
 })
 
 port = process.env.PORT || 8080
 
 app.listen(port, function () {
-  console.log('Example app listening on port '+port)
+	console.log('Example app listening on port ' + port)
 })
